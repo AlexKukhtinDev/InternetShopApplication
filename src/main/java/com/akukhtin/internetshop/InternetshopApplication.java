639c6572ef8784cb997af480dae8b4cafcf64d98
akukhtin <alex.kukhtin.dev@gmail.com>
2020-04-18 19:44:18 +0300

    Added EmailService and some Impl

--- src/main/java/com/akukhtin/internetshop/InternetshopApplication.java
+++ src/main/java/com/akukhtin/internetshop/InternetshopApplication.java
@@ -4,11 +4,14 @@ import com.akukhtin.internetshop.model.Draft;
 import com.akukhtin.internetshop.repository.DraftRepository;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Properties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -28,6 +31,25 @@ public class InternetshopApplication implements CommandLineRunner {
                 .paths(PathSelectors.any())
                 .build();
     }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("my.gmail@gmail.com");
+        mailSender.setPassword("password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(InternetshopApplication.class, args);
     }
---
+++ src/main/java/com/akukhtin/internetshop/service/implementation/EmailServiceImpl.java
@@ -0,0 +1,34 @@
+package com.akukhtin.internetshop.service.implementation;
+
+import com.akukhtin.internetshop.service.EmailService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+    public EmailServiceImpl(@Qualifier("mailSender") JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    @Bean
+    public SimpleMailMessage templateSimpleMessage() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setText(
+                "This is the test email template for your email:\n%s\n");
+        return message;
+    }
+
+    public final JavaMailSender emailSender;
+
+    @Override
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
